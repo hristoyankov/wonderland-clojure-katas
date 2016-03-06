1e360f027a0178669b89b44016f3985dc4783268
Hristo Yankov <ico.yankov@gmail.com>
2016-03-06 15:12:25 +0200

    Implemented encode.

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,19 @@
 (ns alphabet-cipher.coder)
 
+(defn char->index [c]
+  (- (int c) (int \a)))
+
+(defn rotate [n xs]
+  (concat (drop n xs) (take n xs)))
+
+(def alphabet
+  (iterate #(rotate 1 %) (seq "abcdefghijklmnopqrstuvwxyz")))
+
 (defn encode [keyword message]
-  "encodeme")
+  (let [key (take (count message) (cycle keyword))
+        msg (seq message)]
+    (apply str (map #(nth (nth alphabet (char->index %1)) (char->index %2))
+                    msg key))))
 
 (defn decode [keyword message]
   "decodeme")
