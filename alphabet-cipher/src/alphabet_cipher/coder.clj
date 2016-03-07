4900e7495305637b50919d5a2db781852cb649e0
Hristo Yankov <ico.yankov@gmail.com>
2016-03-07 22:19:13 +0200

    Refactor encode & decode.

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,27 +1,21 @@
 (ns alphabet-cipher.coder)
 
-(defn char->index [c]
-  (- (int c) (int \a)))
-
-(def alphabet (seq "abcdefghijklmnopqrstuvwxyz"))
-
-(defn encode [keyword message]
+(defn coder [keyword message f]
   (let [key (take (count message) (cycle keyword))
-        msg (seq message)]
+        msg (seq message)
+        alphabet (seq "abcdefghijklmnopqrstuvwxyz")
+        char->index (fn [c] (- (int c) (int \a)))]
     (apply str (map #(nth alphabet
-                          (mod (+ (char->index %2)
-                                  (char->index %1))
-                               (count alphabet)))
-                    msg key))))
-
-(defn decode [keyword message]
-  (let [key (take (count message) (cycle keyword))
-        msg (seq message)]
-    (apply str (map #(nth alphabet
-                          (mod (- (char->index %1)
+                          (mod (f (char->index %1)
                                   (char->index %2))
                                (count alphabet)))
                     msg key))))
+
+(defn encode [keyword message]
+  (coder keyword message +))
+
+(defn decode [keyword message]
+  (coder keyword message -))
 
 (defn decipher [cipher message]
   "decypherme")
