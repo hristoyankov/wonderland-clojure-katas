40d1a43f4d95e8f110699ff52d23846a7a013b41
Hristo Yankov <ico.yankov@gmail.com>
2016-03-08 09:05:42 +0200

    Implement wonderland-number.

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,17 @@
 (ns wonderland-number.finder)
 
+(defn hasAllTheSameDigits? [n1 n2]
+  (let [s1 (set (str n1))
+        s2 (set (str n2))]
+    (= s1 s2)))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (first (->> (iterate inc 1)
+              (filter #(> % 99999))
+              (filter #(< % 1000000))
+              (filter #(hasAllTheSameDigits? % (* 2 %)))
+              (filter #(hasAllTheSameDigits? % (* 3 %)))
+              (filter #(hasAllTheSameDigits? % (* 4 %)))
+              (filter #(hasAllTheSameDigits? % (* 5 %)))
+              (filter #(hasAllTheSameDigits? % (* 6 %))))))
+
