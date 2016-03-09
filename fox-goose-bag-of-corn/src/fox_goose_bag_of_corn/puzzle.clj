20172dc0e1b9b76b53a815f29a311f1c906a92fb
Hristo Yankov <ico.yankov@gmail.com>
2016-03-09 21:37:21 +0200

    Lame solution to fox-goose-corn.

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -1,6 +1,20 @@
 (ns fox-goose-bag-of-corn.puzzle)
 
-(def start-pos [[[:fox :goose :corn :you] [:boat] []]])
+(def start-pos [[[:fox :goose :corn :you] [:boat] []]
+                [[:corn :fox] [:you :goose :boat] []]
+                [[:corn :fox] [:boat] [:you :goose]]
+                [[:corn :fox] [:you :boat] [:goose]]
+                [[:corn :fox :you] [:boat] [:goose]]
+                [[:corn] [:boat :you :fox] [:goose]]
+                [[:corn] [:boat] [:you :fox :goose]]
+                [[:corn] [:boat :you :goose] [:fox]]
+                [[:corn :you :goose] [:boat] [:fox]]
+                [[:goose] [:boat :you :corn] [:fox]]
+                [[:goose] [:boat] [:fox :you :corn]]
+                [[:goose] [:boat :you] [:fox :corn]]
+                [[:goose :you] [:boat] [:fox :corn]]
+                [[] [:boat :you :goose] [:fox :corn]]
+                [[] [:boat] [:fox :goose :corn :you]]])
 
 (defn river-crossing-plan []
   start-pos)
