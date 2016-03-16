b76f7711e2e879a122661d2294deee4dc127d0b4
Hristo Yankov <ico.yankov@gmail.com>
2016-03-16 08:39:05 +0200

    Add hamming distance, children & build-tree funcs.

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -7,5 +7,25 @@
                (slurp)
                (read-string)))
 
+(defn hamming
+  [word1 word2]
+  (when (= (count word1)
+           (count word2))
+    (count (remove #(= (first %) (second %))
+                   (partition 2 (interleave word1 word2))))))
+
+(defn children
+  [word dictionary]
+  (filter #(= 1 (hamming word %)) dictionary))
+
+(defn build-tree
+  [word dict]
+  (let [dict  (remove #(= word %) dict)
+        nodes (children word dict)]
+    (concat (list word)
+            (map #(build-tree % (clojure.set/difference (set dict) (set nodes))) nodes))))
+
 (defn doublets [word1 word2]
-  "make me work")
+  (map first (tree-seq next rest (build-tree word1 words))))
+
+
